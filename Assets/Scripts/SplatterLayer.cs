6672c09250c076ed128bfb3b75abb0de822edb34
Sam Orme <ormesam@hotmail.com>
2021-06-30 23:04:51 +0100

    Restrict angle limits

--- Assets/Scripts/SplatterLayer.cs
+++ Assets/Scripts/SplatterLayer.cs
@@ -8,7 +8,11 @@ namespace Splatter {
         public TerrainLayer Layer;
         public float MinHeight = 0;
         public float MaxHeight = Mathf.Infinity;
+
+        [Range(0, 90)]
         public float MinAngle = 0;
+
+        [Range(0, 90)]
         public float MaxAngle = 90;
     }
 }
