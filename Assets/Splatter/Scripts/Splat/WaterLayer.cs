761a055b8021f9a1114b20f13ca3c0880a75a3ac
Sam Orme <ormesam@hotmail.com>
2021-08-19 17:41:44 +0100

    Update water height even if ResizeToTerrain is not checked

--- Assets/Splatter/Scripts/Splat/WaterLayer.cs
+++ Assets/Splatter/Scripts/Splat/WaterLayer.cs
@@ -26,15 +26,20 @@ namespace Splatter {
             }
 
             if (WaterObject && ResizeToTerrain) {
-                WaterObject.transform.position = new Vector3(
-                    terrain.transform.position.x + (terrain.terrainData.size.x * 0.5f),
-                    terrain.transform.position.y + WaterHeight,
-                    terrain.transform.position.z + (terrain.terrainData.size.z * 0.5f));
-
                 WaterObject.transform.localScale = new Vector3(
                     terrain.terrainData.size.x * 0.1f,
                     WaterObject.transform.localScale.y,
                     terrain.terrainData.size.z * 0.1f);
+
+                WaterObject.transform.position = new Vector3(
+                    terrain.transform.position.x + (terrain.terrainData.size.x * 0.5f),
+                    terrain.transform.position.y + WaterHeight,
+                    terrain.transform.position.z + (terrain.terrainData.size.z * 0.5f));
+            } else {
+                WaterObject.transform.position = new Vector3(
+                    WaterObject.transform.position.x,
+                    WaterHeight,
+                    WaterObject.transform.position.z);
             }
         }
     }
